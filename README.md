ca58afb996599f2da4403cf1a19015b76be1a606
Hondallica <hondallica@gmail.com>
2014-09-23 20:38:57 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WebService::Rockol - It's new $module
+
+# SYNOPSIS
+
+    use WebService::Rockol;
+
+# DESCRIPTION
+
+WebService::Rockol is ...
+
+# LICENSE
+
+Copyright (C) Hondallica.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Hondallica <hondallica@gmail.com>
---
+++ lib/WebService/Rockol.pm
@@ -0,0 +1,39 @@
+package WebService::Rockol;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::Rockol - It's new $module
+
+=head1 SYNOPSIS
+
+    use WebService::Rockol;
+
+=head1 DESCRIPTION
+
+WebService::Rockol is ...
+
+=head1 LICENSE
+
+Copyright (C) Hondallica.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Hondallica E<lt>hondallica@gmail.comE<gt>
+
+=cut
+
